e943bc0bae0108e417203c5ed68803ad91805162
Rabin Gharti Magar <rabin_mgr@live.com>
2020-10-17 18:08:07 +0100

    File modified

--- js/theme.js
+++ js/theme.js
@@ -1,85 +1,90 @@
-// Body
-const body = document.querySelector("body");
-// Header
-const header = document.querySelector("header");
-// Wrapper
-const wrap = document.querySelector(".wrapper");
-// Toggle button
-const btn = document.querySelector(".switch-light");
-// Search icon
-const searchIcon = document.querySelector(".icon-search");
-// Footer
-const footer = document.querySelector("footer");
+const btn = document.querySelector('#toggle');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const wrap = document.querySelector('.wrapper');
+const searchIcon = document.querySelector('.icon-search');
+const error = document.querySelector('.error-msg');
+const footer = document.querySelector('footer');
 
-// Background color
+// Background Color
 const setBGColor = (element, color) => (element.style.backgroundColor = color);
-// color
+// Color
 const setColor = (element, color) => (element.style.color = color);
 // SVG Fill
 const svgFill = (element, color) => (element.style.fill = color);
 
 // Color
-const black = "#000";
-const codGray = "#111";
-const white = "#fff";
+const black = '#000';
+const codGray = '#111';
+const white = '#fff';
 
-// Function
-const checkedBtn = e => {
-  const theTarget = e.target;
-  const employeeDir = document.querySelectorAll(".employee-dir");
+const darkMode = () => {
+  // Background Color
+  setBGColor(body, codGray);
+  setBGColor(header, black);
+  setBGColor(wrap, black);
+  setBGColor(modal, codGray);
+  setBGColor(search, codGray);
+  // Color
+  setColor(modal, white);
+  setColor(modalClose, white);
+  setColor(footer, white);
+  setColor(search, white);
+  setColor(error, white);
+  // Border Style
+  search.style.border = '1px solid white';
+  // SVG Fill
+  svgFill(leftArrow, white);
+  svgFill(rightArrow, white);
+  svgFill(searchIcon, white);
+  // Loop
+  const employeeDir = document.querySelectorAll('.employee-dir');
 
-  if (theTarget.checked) {
+  employeeDir.forEach(employee => {
     // Background Color
-    setBGColor(body, codGray);
-    setBGColor(header, black);
-    setBGColor(wrap, black);
-    setBGColor(modal, codGray);
-    setBGColor(search, codGray);
+    setBGColor(employee, codGray);
     // Color
-    setColor(modal, white);
-    setColor(modalClose, white);
-    setColor(footer, white);
+    setColor(employee, white);
     // Border Style
-    search.style.border = "1px solid white";
-    // SVG Fill
-    svgFill(leftArrow, white);
-    svgFill(rightArrow, white);
-    svgFill(searchIcon, white);
+    employee.style.border = '1px solid #fff';
+  });
+};
 
-    employeeDir.forEach(employee => {
-      // Background Color
-      setBGColor(employee, codGray);
-      // Color
-      setColor(employee, white);
-      // Border Style
-      employee.style.border = "1px solid #fff";
-    });
-  } else {
+const lightMode = () => {
+  // Background Color
+  setBGColor(body, '');
+  setBGColor(header, '');
+  setBGColor(wrap, '');
+  setBGColor(modal, '');
+  setBGColor(search, '');
+  // Color
+  setColor(modal, '');
+  setColor(modalClose, '');
+  setColor(footer, '');
+  setColor(search, '');
+  setColor(error, '');
+  // Border Style
+  search.style.border = '';
+  // SVG Fill
+  svgFill(leftArrow, '');
+  svgFill(rightArrow, '');
+  svgFill(searchIcon, '');
+  // Loop
+  const employeeDir = document.querySelectorAll('div.employee-dir');
+
+  employeeDir.forEach(employee => {
     // Background Color
-    setBGColor(body, "");
-    setBGColor(header, "");
-    setBGColor(wrap, "");
-    setBGColor(modal, "");
-    setBGColor(search, "");
+    setBGColor(employee, '');
     // Color
-    setColor(modal, "");
-    setColor(modalClose, "");
-    setColor(footer, "");
-    // Border Style
-    search.style.border = "";
-    // SVG Fill
-    svgFill(leftArrow, "");
-    svgFill(rightArrow, "");
-    svgFill(searchIcon, "");
-
-    employeeDir.forEach(employee => {
-      // Background Color
-      setBGColor(employee, "");
-      // Color
-      setColor(employee, "");
-    });
-  }
+    setColor(employee, '');
+  });
 };
 
 // Event Listener
-btn.addEventListener("click", checkedBtn);
+btn.addEventListener('click', () => {
+  if (btn.checked) {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
